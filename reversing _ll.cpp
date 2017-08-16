df95b88be9ace58fdced0e04aa4536b000b149bb
divyanshu <divyanshu.r46956@gmail.com>
2017-08-16 16:06:11 +0530

    added reversing_ll.cpp

---
+++ reversing _ll.cpp
@@ -0,0 +1,138 @@
+
+
+// Add a node at the front: (A 4 steps process)
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+struct Node_prototype{
+	int data;
+	struct Node_prototype *next_ptr;
+};
+
+void push(struct Node_prototype **head_node,int new_data){
+
+	struct Node_prototype* new_node = (struct Node_prototype*)malloc(sizeof(struct Node_prototype));
+
+	new_node->data = new_data;
+	new_node->next_ptr = (*head_node);
+	(*head_node) = new_node;
+
+}
+
+void print_linked_list(struct Node_prototype *head_node){
+
+	while(head_node != NULL){
+
+		printf("%d\n", head_node->data);
+		head_node  = head_node->next_ptr;
+
+	}
+
+}
+
+void delete_node(struct Node_prototype **head_node,int to_be_deleted){
+  
+    struct Node_prototype* temp = *head_node;
+    struct Node_prototype* prev_node;
+
+	if(temp->data == to_be_deleted){
+		*head_node = temp->next_ptr;
+		free(temp);
+		return;
+	}    
+
+	while(temp->data != to_be_deleted){
+
+		prev_node = temp;
+		temp = temp->next_ptr;
+
+	}
+
+	prev_node->next_ptr = temp->next_ptr;
+	free(temp);
+}
+
+void ll_reversal(struct Node_prototype **head_node){
+
+	struct Node_prototype* temp = *head_node;
+	struct Node_prototype* prev = NULL;
+	struct Node_prototype* current = *head_node;
+
+	while(current != NULL){
+
+		temp = current;
+		current = temp->next_ptr;
+		temp->next_ptr = prev;
+		prev = temp;	
+	}
+
+	*head_node = prev;
+
+
+}
+
+
+int getCount(struct Node_prototype *head_node){
+
+	int count;
+	struct Node_prototype* temp = head_node;
+
+	while(temp !=NULL){
+		count++;
+		temp = temp->next_ptr;
+	}
+
+	return count;
+
+}
+
+int main(){
+
+
+	struct Node_prototype *node_one = NULL;
+	struct Node_prototype *node_two = NULL;
+	struct Node_prototype *node_three = NULL;
+	int count;
+	// struct Node_prototype *head_ptr->next_ptr = node_one
+
+	node_one = (struct Node_prototype*) malloc(sizeof(struct Node_prototype));
+	node_two = (struct Node_prototype*) malloc(sizeof(struct Node_prototype));
+	node_three = (struct Node_prototype*) malloc(sizeof(struct Node_prototype));
+
+	// head_node->next_ptr = node_one;
+
+
+	node_one->data = 1;
+	node_one->next_ptr = node_two;
+
+	node_two->data = 2;
+	node_two->next_ptr = node_three;
+
+	node_three->data = 3;
+	node_three->next_ptr = NULL;
+
+	push(&node_one,4);
+	push(&node_one,5);
+	push(&node_one,6);
+
+	// delete_node(&node_one,6);
+	// delete_node(&node_one,2);
+	// delete_node(&node_one,3);
+
+	// count = getCount(node_one);
+
+	// printf("%d\n",count );
+
+	print_linked_list(node_one);
+
+	printf("Updated print_linked_list: \n");
+
+	ll_reversal(&node_one);
+
+	print_linked_list(node_one);
+
+}
+
